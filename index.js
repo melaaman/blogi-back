db6e08d27b3746131d86ed66a14806cd334057a2
melaaman <erika.laamanen@gmail.com>
2020-03-01 15:47:53 +0200

    Fix port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/api/texts', (__, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
